362401ec8f3999573b1d5c14f25ad95bf92a3e5c
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-09-06 17:19:14 -0500

    Fixes error on CircleCi Configuration

--- frontend/emi/acss/i18n/en.ts
+++ frontend/emi/acss/i18n/en.ts
@@ -1,6 +1,18 @@
 export const locale = {
   lang: 'en',
   data: {
-    
+    ACSS: {
+      ACSS: 'Automated clearing and settlement system'
+    },
+    ERRORS: {
+      16001: 'Internal server error',
+      16002: 'Permission denied',
+      16010: 'User missing data',
+      16011: 'The username is already used',
+      16012: 'invalid username, the username must contain at least 8 characters and be composed of at least one (1) number and one (1) lowercase letter.',
+      16014: 'The email is already used',
+      16015: 'Permission denied, it is not possible to update your own users information',
+      16016: 'Usuario no encontrado'
+    }
   }
 };
--- frontend/emi/acss/i18n/es.ts
+++ frontend/emi/acss/i18n/es.ts
@@ -1,6 +1,18 @@
 export const locale = {
   lang: 'es',
   data: {
-
+    ACSS: {
+      ACSS: 'Sistema automatizado de compensación y liquidación'
+    },
+    ERRORS: {
+      16001: 'Error interno de servidor',
+      16002: 'Permiso denegado.',
+      16010: 'Datos faltantes del usuario',
+      16011: 'El nombre de usuario ya está siendo usado',
+      16012: 'Nombre de usuario invalido, el nombre de usuario debe contener como mínimo 8 caracteres y estar compuesto por al menos un (1) número y una (1) letra minúscula.',
+      16014: 'El email ya está siendo usado',
+      16015: 'Permiso denegado, no es posible actualizar la información de su propio usuario',
+      16016: 'Usuario no encontrado'
+    }
   }
 };
